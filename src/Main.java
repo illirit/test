2f02f08e59bf5425c22b33f6c5db478a77f56ca5
illirit <roman.yakunov@yandex.ru>
2017-08-09 00:19:13 +0300

    w

--- src/Main.java
+++ src/Main.java
@@ -11,7 +11,6 @@ public class Main {
 
 
 	public static void main(String[] args){
-
 		OMFactory factory = OMAbstractFactory.getOMFactory();
 		OMElement method = factory.createOMElement("getPriceResponse", "23", null);
 		OMElement value = factory.createOMElement("price", "24", null);
