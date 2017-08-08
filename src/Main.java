069de20dec2e5d916326b66bc48cdb05151d8d85
illirit <roman.yakunov@yandex.ru>
2017-08-08 22:51:41 +0300

    1

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+import org.apache.axiom.*;
+import org.apache.axiom.om.OMAbstractFactory;
+import org.apache.axiom.om.OMElement;
+import org.apache.axiom.om.OMFactory;
+import org.apache.axiom.soap.SOAPEnvelope;
+import org.apache.axiom.soap.SOAPFactory;
+
+
+
+public class Main {
+
+
+	public static void main(String[] args){
+
+		OMFactory factory = OMAbstractFactory.getOMFactory();
+		OMElement method = factory.createOMElement("getPriceResponse", "23", null);
+		OMElement value = factory.createOMElement("price", "24", null);
+        value.addChild(factory.createOMText(value, ""));
+        method.addChild(value);
+		System.out.println(method);
+		
+
+    }
+}
